d9ca0efc84cbd9492ab8811dc5b8e031b07b5609
aquapi <aquaplmc@gmail.com>
2023-01-23 21:04:20 +0700

    Update

--- assets/examples/app.ts
+++ assets/examples/app.ts
@@ -1,6 +1,12 @@
 import { App } from "@bunsvr/core";
 import { Router } from "../..";
 
+// Return 404 for other routes
+const app = new App().use(async req =>
+    new Response(`Cannot ${req.method} ${req.url}`, { status: 404 })
+);
+
+// Serve the returned app using Bun
 export default new Router()
     // Add a handler to route / (All method)
     .static("", "/", () =>
@@ -8,9 +14,5 @@ export default new Router()
     // Add a handler to dynamic route /user/:id (GET method only)
     .dynamic("GET", "/user/:id", req =>
         new Response(req.params?.[1] || ""))
-    // Register as a middleware
-    .register(
-        // Return 404 for other routes
-        new App().use(async req =>
-            new Response(`Cannot ${req.method} ${req.url}`, { status: 404 })
-        ));
+    // Register as a middleware and returns the app
+    .register(app);
