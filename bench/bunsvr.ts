9c2d05f3f3e04c15151f15ae59343c5731c50e23
Revenity <87530663+aquapi@users.noreply.github.com>
2023-01-23 21:02:34 +0700

    Update

--- bench/bunsvr.ts
+++ bench/bunsvr.ts
@@ -4,8 +4,7 @@ new Router()
     .static("GET", "/", 
         () => new Response("Hi"))
     .static("POST", "/json",
-        async req => Response.json(await req.json())
-    )
+        async req => Response.json(await req.json()))
     .dynamic("GET", "/id/:id", 
         req => new Response(req.params?.[1]))
     .serve();
