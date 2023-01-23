adea7ba2571d793ae2857de31cca7f5c3942ef5a
Revenity <87530663+aquapi@users.noreply.github.com>
2023-01-23 21:02:09 +0700

    Update

--- assets/examples/app.ts
+++ assets/examples/app.ts
@@ -4,16 +4,13 @@ import { Router } from "../..";
 export default new Router()
     // Add a handler to route / (All method)
     .static("", "/", () =>
-        new Response("Hello!")
-    )
+        new Response("Hello!"))
     // Add a handler to dynamic route /user/:id (GET method only)
     .dynamic("GET", "/user/:id", req =>
-        new Response(req.params?.[1] || "")
-    )
+        new Response(req.params?.[1] || ""))
     // Register as a middleware
     .register(
         // Return 404 for other routes
         new App().use(async req =>
             new Response(`Cannot ${req.method} ${req.url}`, { status: 404 })
-        )
-    );
+        ));
--- assets/examples/index.ts
+++ assets/examples/index.ts
@@ -2,8 +2,10 @@ import { Router } from "../..";
 
 new Router()
     // Add a handler to route "/home"
-    .static("GET", "/home", () => new Response("Hello!"))
+    .static("GET", "/home", () => 
+        new Response("Hello!"))
     // Return a 404 for all other routes
-    .dynamic("", "/(.*)", () => new Response("Not Found"))
+    .dynamic("", "/(.*)", () => 
+        new Response("Not Found"))
     // Serve directly
     .serve();
