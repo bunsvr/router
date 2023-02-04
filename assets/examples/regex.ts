85c2d84f4194a970ff637ddc92d78a42d1f3e3f0
Revenity <87530663+aquapi@users.noreply.github.com>
2023-02-04 14:04:15 +0700

    Update examples

--- assets/examples/regex.ts
+++ assets/examples/regex.ts
@@ -3,15 +3,12 @@ import { Router } from "../..";
 // Match '/a' and '/b'
 const valid = /\/(a|b)/;
 
-const router = new Router()
+new Router()
     // Add a handler to route "/home"
     .static(["GET", "POST"], "/home", () => 
         new Response("Hello!"))
     // Handle path that match the RegExp
     .dynamic(["GET", "DELETE"], valid, req =>
         new Response(req.params.join(" ")))
-    
-console.log(router)
-
-router// Serve directly
-.serve();
+    // Serve directly
+    .serve();
